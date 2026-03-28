c3f68d22ebdb57dbc85e7b314b4cc561dd563cbc
Pedro Rojas <pedro.rojaslo50@gmail.com>
2026-03-28 09:26:07 -0700

    Remove black background

--- ClockApp/Timers/Shared/Views/LockScreenTimerLiveActivityView.swift
+++ ClockApp/Timers/Shared/Views/LockScreenTimerLiveActivityView.swift
@@ -29,7 +29,6 @@ struct LockScreenTimerLiveActivityView: View {
                 .frame(maxWidth: .infinity, alignment: .trailing)
         }
         .padding()
-        .background(Color.black)
     }
 }
 
